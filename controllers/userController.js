92c4e82fda998dc9a3dbc5c03fa5546888e6e584
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-03-04 11:53:59 +0530

    add change

--- controllers/userController.js
+++ controllers/userController.js
@@ -458,6 +458,17 @@ const verifyRefralcode=async(req,res)=>{
     }
 }
 
+const apitest=async(req,res)=>{
+    try{
+        res.json({
+            msg:"mc bsdk chal bhi le",
+            status:200
+        })
+    }catch(error){
+        console.log(error);
+    }
+}
+
 export {
     registerUser,
     verifyOtp,
@@ -468,4 +479,5 @@ export {
     logOut,
     changePassword,
     verifyRefralcode,
+    apitest
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,7 @@ import {
     changePassword,
     verifyOtp,
     verifyRefralcode,
+    apitest
 } from '../controllers/userController.js';
 import {
     authenticateUser,
@@ -35,5 +36,6 @@ router.post('/changePassword', authenticateUser, validateRequest(setPasswordVali
 router.post('/verifyOTP', validateRequest(userValidationSchemaOTP),verifyOtp);
 router.post('/refreshToken', refreshToken);
 router.post('/verifyRefral', verifyRefralcode);
+router.get('/',apitest)
 
 export default router;
