e9782e87ccef4e182064cbddf36195ba21c8aaa4
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-03-04 13:35:46 +0530

    Add change

--- config/loadConfig.js
+++ config/loadConfig.js
@@ -55,7 +55,28 @@ const loadConfig = async () => {
       const secrets = await getSecrets();
 
       logger.info('Secrets successfully loaded from AWS.');
-      return { ...secrets };
+
+      // return { ...config, ...secrets };
+      config = {
+        NODE_ENV: secrets.process.env.NODE_ENV,
+        PORT: secrets.process.env.PORT,
+        DB_URI: secrets.process.env.DB_URI,
+        ACCESS_TOKEN_SECRET: secrets.process.env.ACCESS_TOKEN_SECRET,
+        REFRESH_TOKEN_SECRET: secrets.process.env.REFRESH_TOKEN_SECRET,
+        AWS_REGION: process.env.AWS_REGION || 'us-east-1',
+        SECRET_NAME: process.env.SECRET_NAME || 'social-com',
+        TWILIO_ACCOUNT_SID: secrets.process.env.twilio_account_sid,
+        TWILIO_AUTH_TOKEN: secrets.process.env.twilio_auth_token,
+        TWILIO_SERVICE_SID: process.env.twilio_service_sid,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+        APPLE_CLIENT_ID: process.env.APPLE_CLIENT_ID,
+        APPLE_TEAM_ID: process.env.APPLE_TEAM_ID,
+        APPLE_KEY_ID: process.env.APPLE_KEY_ID,
+        APPLE_PRIVATE_KEY: process.env.APPLE_PRIVATE_KEY,
+        APPLE_CALLBACK_URL: process.env.APPLE_CALLBACK_URL,
+      };
     } catch (error) {
       logger.error(`Error fetching secrets from AWS Secrets Manager: ${error.message}`);
       process.exit(1);
