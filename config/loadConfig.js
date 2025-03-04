0e070eaeb8ba21c66bb0cdb7d1bc9de95d01ecab
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-03-04 14:31:12 +0530

    add chnage

--- config/loadConfig.js
+++ config/loadConfig.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const ENV = process.env.NODE_ENV || "production";
 const REGION = process.env.AWS_REGION || "us-east-1";
-// const SECRET_NAME = process.env.SECRET_NAME || "social-com";
+const SECRET_NAME = process.env.SECRET_NAME || "social-com";
 const secretsManager = new SecretsManagerClient({ region: REGION });
 
 
