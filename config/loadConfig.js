8827dbdf5c555972ca56505f297422439f9daf82
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-03-04 14:41:44 +0530

    add chnage load config

--- config/loadConfig.js
+++ config/loadConfig.js
@@ -9,22 +9,6 @@ const REGION = process.env.AWS_REGION || "us-east-1";
 const SECRET_NAME = process.env.SECRET_NAME || "social-com";
 const secretsManager = new SecretsManagerClient({ region: REGION });
 
-
-// async function getSecrets() {
-//   try {
-//     const response = await secretsManager.send(
-//       new GetSecretValueCommand({ SecretId: SECRET_NAME })
-//     );
-
-//     if (response.SecretString) {
-//       return JSON.parse(response.SecretString);
-//     }
-//   } catch (error) {
-//     console.error("Error retrieving secrets from AWS Secrets Manager:", error);
-//     throw error;
-//   }
-// }
-
 const loadConfig = async () => {
   if (ENV === 'production') {
     try {
@@ -66,28 +50,28 @@ const loadConfig = async () => {
     }
   }
 
-  // logger.info('Running in development mode. Using .env for configuration.');
+  logger.info('Running in development mode. Using .env for configuration.');
 
-  // return {
-  //   NODE_ENV: process.env.NODE_ENV || 'development',
-  //   PORT: process.env.PORT || 3030,
-  //   DB_URI: process.env.DB_URI,
-  //   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-  //   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-  //   AWS_REGION: process.env.AWS_REGION || 'us-east-1',
-  //   SECRET_NAME: process.env.SECRET_NAME || 'social-com',
-  //   TWILIO_ACCOUNT_SID: process.env.twilio_account_sid,
-  //   TWILIO_AUTH_TOKEN: process.env.twilio_auth_token,
-  //   TWILIO_SERVICE_SID: process.env.twilio_service_sid,
-  //   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  //   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  //   GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
-  //   APPLE_CLIENT_ID: process.env.APPLE_CLIENT_ID,
-  //   APPLE_TEAM_ID: process.env.APPLE_TEAM_ID,
-  //   APPLE_KEY_ID: process.env.APPLE_KEY_ID,
-  //   APPLE_PRIVATE_KEY: process.env.APPLE_PRIVATE_KEY,
-  //   APPLE_CALLBACK_URL: process.env.APPLE_CALLBACK_URL,
-  // }
+  return {
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 3030,
+    DB_URI: process.env.DB_URI,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    AWS_REGION: process.env.AWS_REGION || 'us-east-1',
+    SECRET_NAME: process.env.SECRET_NAME || 'social-com',
+    TWILIO_ACCOUNT_SID: process.env.twilio_account_sid,
+    TWILIO_AUTH_TOKEN: process.env.twilio_auth_token,
+    TWILIO_SERVICE_SID: process.env.twilio_service_sid,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+    APPLE_CLIENT_ID: process.env.APPLE_CLIENT_ID,
+    APPLE_TEAM_ID: process.env.APPLE_TEAM_ID,
+    APPLE_KEY_ID: process.env.APPLE_KEY_ID,
+    APPLE_PRIVATE_KEY: process.env.APPLE_PRIVATE_KEY,
+    APPLE_CALLBACK_URL: process.env.APPLE_CALLBACK_URL,
+  }
 };
 
 export { loadConfig };
